e490cc875343308c1bd246a3ce82da2fdb527c3c
hristo222h <hristomirhristov09@gmail.com>
2025-11-10 02:49:04 +0300

    Списък от прости числа

---
+++ Списък от прости числа/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Списък_от_прости_числа
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Въведи до колко да е списъкът: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+
+            for (int i = 2; i <= n; i++)
+            {
+                int flag = 0;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0)
+                {
+                    list.Add(i);
+                }
+            }
+
+            Console.WriteLine("Списък от прости числа");
+            foreach (int chislo in list)
+            {
+                Console.Write(chislo + " ");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
+
+
